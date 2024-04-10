3773a1843f3bdb44d2a89fd8d25eb5bcc5e0ddf2
Vitor Antonionne <vitor-antonionne@hotmail.com>
2024-04-10 16:29:56 -0300

    complete :tada:

--- src/exerc.ts
+++ src/exerc.ts
@@ -45,33 +45,55 @@ async function userGitResponse(username: string) {
   } else {
     const  response = await fetch(user.repos_url)
     const userRepos: gitHubRepoResponse[] = await response.json()
-
-    let message = `Id: ${user.id}
-                      \nLogin: ${user.login}
-                      \nNome: ${user.name}
-                      \nBio: ${user.bio}
-                      \nRepositório Públicos: ${user.public_repos}`
     
-    userRepos.forEach(repos =>
+    let message = `Id: ${user.id}
+    \nLogin: ${user.login}
+    \nNome: ${user.name}
+    \nBio: ${user.bio}
+    \nRepositório Públicos: ${user.public_repos}`
+    
+    userRepos.forEach(repos => {
       message +=  `\nNome: ${repos.name}
-                   \nDescrição: ${repos.description}
-                   \nEstrelas: ${repos.stargazers_count}
-                   \nÉ um fork: ${repos.fork}
+      \nDescrição: ${repos.description}
+      \nEstrelas: ${repos.stargazers_count}
+      \nÉ um fork: ${repos.fork}
       `
-    )
+    })   
+    
+    console.log(message);
   }
 }
 
-function usersList() {
-  let message = ''
-
-  users.forEach((personUser) => {
-    message += `
+function usersList() { 
+   users.forEach((personUser) => {
+    let message = `
             \nId: ${personUser.id}
             \nlogin: ${personUser.login}
             \nNome: ${personUser.name}
             \nBio: ${personUser.bio}
             \nRepositórios públicos: ${personUser.public_repos}
     `
+    console.log(message);
   })
+}
+
+function showUserTotal() {
+  const repoTotal = users.map((personRepo) => {
+    personRepo.public_repos = personRepo.public_repos + personRepo.public_repos
+    return personRepo.public_repos
+  })
+
+  const totalRepo = repoTotal.reduce((a, b) => a + b, 0)
+  return totalRepo
+}
+
+function listRepoTopFive() {
+  const topFive = users.slice().sort((a,b) => b.public_repos - a.public_repos).slice(0, 5)
+  let textTopFive = `Top 5 usuários com mais repositórios
+                    \nPosição --- Nome --- Repositório`
+
+  topFive.forEach((userItem, index) => {
+    textTopFive +=  `\n${index + 1} = ${userItem.name} - ${userItem.public_repos}`
+  })
+  console.log(textTopFive);
 }
